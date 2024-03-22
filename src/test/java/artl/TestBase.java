456cf684c05ad43099d120e61f579a9c3bec045c
artl <artemlarichev.v@gmail.com>
2024-03-22 13:53:17 +0100

    Added DesiredCapabilities in TestBase

--- src/test/java/artl/TestBase.java
+++ src/test/java/artl/TestBase.java
@@ -5,6 +5,7 @@ import artl.config.WebDriverConfig;
 import com.codeborne.selenide.Configuration;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Objects;
 
@@ -20,7 +21,10 @@ public class TestBase {
         Configuration.browser = webDriverConfig.getBrowser();
         Configuration.browserVersion = webDriverConfig.getBrowserVersion();
         Configuration.browserSize = webDriverConfig.getBrowserSize();
-        Configuration.remote = null; /*String.valueOf(webDriverConfig.getRemoteUrl());*/
+        Configuration.remote = /*null;*/ String.valueOf(webDriverConfig.getRemoteUrl());
+        Configuration.pageLoadStrategy = "eager";
+
+
 
 
         String baseUrlOfPage = System.getProperty("base.url");
@@ -28,6 +32,11 @@ public class TestBase {
             baseUrlOfPage= "https://github.com/";
         }
         open(baseUrlOfPage);
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+
+
     }
 
 }

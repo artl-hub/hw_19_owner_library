444fd4c73c02751c61a6c2058420d4c1de80dd07
artl <artemlarichev.v@gmail.com>
2024-03-30 12:28:49 +0100

    deleted unused import

--- src/test/java/artl/tests/GitHubTest.java
+++ src/test/java/artl/tests/GitHubTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Configuration.baseUrl;
+
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.title;
 import static com.codeborne.selenide.logevents.SelenideLogger.step;
